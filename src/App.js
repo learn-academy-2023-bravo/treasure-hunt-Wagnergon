cff821320a87ee1b0d2dd718503b62a347793c83
wagnergon <wagnergon52@gmail.com>
2023-04-03 10:40:03 -0700

    user stories 2

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react"
 import "./App.css"
+import Square from './components/Square'
 
 const App = () => {
   const [board, setBoard] = useState([
@@ -14,9 +15,24 @@ const App = () => {
     "?"
   ])
 
+  const handleGameplay = (clickedSquare) => {
+    alert(clickedSquare)
+  }
+
   return (
     <>
       <h1>Treasure Hunt Game</h1>
+      <div className="board">
+        {
+          board.map(square, index => {
+            return <Square 
+            square={square} 
+            index={index} 
+            key={index}
+            handleGamePlay={handleGamePlay}/>
+          })
+        }
+      </div>
     </>
   )
 }
